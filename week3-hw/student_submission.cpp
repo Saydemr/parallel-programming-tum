7327421c1570f57a93be04a3ba44c6360f623350
Saydemr <37012364+Saydemr@users.noreply.github.com>
2023-05-29 20:36:39 +0200

    latest...

--- week3-hw/student_submission.cpp
+++ week3-hw/student_submission.cpp
@@ -1,16 +1,91 @@
-//
-// Created by Dennis-Florian Herr on 13/06/2022.
-//
-
 #include <string>
 #include <deque>
 #include <future>
 #include <functional>
-#include "Utility.h"
+#include <iostream>
+#include <cstring>
+#include <openssl/sha.h>
+
+
+#define SHA1_BYTES 20
+#define NUM_THREADS 25
+
+struct Sha1Hash {
+    unsigned char data[SHA1_BYTES];
+};
+
+class MyUtility {
+public:
+    static Sha1Hash sha1(std::string input);
+    static Sha1Hash sha1(Sha1Hash input);
+    static Sha1Hash sha1(Sha1Hash hash1, Sha1Hash hash2);
+    static Sha1Hash sha1(const unsigned char* input, size_t input_length);
+};
+
+// generate a sha1 hash from a std::string
+inline Sha1Hash MyUtility::sha1(std::string input){
+    return sha1(reinterpret_cast<const unsigned char*>(input.data()), input.length());
+}
+
+// generate a new Sha1Hash from a Sha1Hash
+inline Sha1Hash MyUtility::sha1(Sha1Hash input){
+    return sha1(input.data, SHA1_BYTES);
+}
+
+// generate a new Sha1 hash by concatenating 2 hashes
+inline Sha1Hash MyUtility::sha1(Sha1Hash hash1, Sha1Hash hash2){
+
+    unsigned char* combinedInput = static_cast<unsigned char*>(malloc(SHA1_BYTES * 2));
+    memcpy(combinedInput, hash1.data, SHA1_BYTES);
+    memcpy(&combinedInput[20], hash2.data, SHA1_BYTES);
+    Sha1Hash result = sha1(combinedInput, SHA1_BYTES*2);
+    return result;
+}
+
+// Wrapper method for creating a Sha1Hash struct using the openssl SHA1 method.
+inline Sha1Hash MyUtility::sha1(const unsigned char* input, size_t input_length){
+    Sha1Hash output;
+
+    // call openssl sha1 method. this requires compilation with -lcrypto
+    SHA1(input, input_length, output.data);
+
+    return output;
+}
+
+inline uint8_t count_leading_zero_bits(Sha1Hash& hash){
+
+    uint32_t bits;
+
+    bits = hash.data[0] << 24 |
+           hash.data[1] << 16 |
+           hash.data[2] << 8 |
+           hash.data[3];
+    if(bits)
+        return __builtin_clz(bits);
+    else
+        return 16;
+}
+
+inline unsigned int readInput() {
+    unsigned int seed = 0;
+    std::cout << "READY" << std::endl;
+    std::cin >> seed;
+
+    return seed;
+}
+
+inline void printHash(Sha1Hash& hash) {
+
+    for (uint8_t i = 0; i < SHA1_BYTES; ++i) {
+        printf("%02x", hash.data[i]);
+    }
+    printf("\nDONE\n");
+}
+
 
 struct Problem {
     Sha1Hash sha1_hash;
-    int problemNum;
+    int16_t problemNum;
 };
 
 
@@ -35,16 +110,10 @@ public:
         return p;
     }
 
-    bool empty(){
-        return problemQueue.empty();
-    }
-    bool stop = false;
-
 private:
     std::deque<Problem> problemQueue;
     std::condition_variable cv;
     std::mutex queue_mutex;
-
 };
 
 ProblemQueue problemQueue;
@@ -56,67 +125,63 @@ ProblemQueue problemQueue;
 inline void generateProblem(int seed){
     srand(seed);
 
-    for(int i = 0; i < 10000; i++){
-        std::string base = std::to_string(rand()) + std::to_string(rand());
-        Sha1Hash hash = Utility::sha1(base);
+    for(int16_t i = 0; i < 10000; ++i){
+        auto str = (std::to_string(rand()) + std::to_string(rand()));
+        Sha1Hash hash = MyUtility::sha1(str);
         do {
-            // we keep hashing ourself until we find the desired amount of leading zeros
-            hash = Utility::sha1(hash);
-        } while(Utility::count_leading_zero_bits(hash) < 9);
+            hash = MyUtility::sha1(hash);
+        } while(count_leading_zero_bits(hash) < 9);
         problemQueue.push(Problem{hash, i});
     }
-    for (int i = 0; i < 23; i++) {
-        problemQueue.push(Problem{Sha1Hash(), -1});
+
+    for (uint8_t i = 0; i < NUM_THREADS; ++i) {
+        problemQueue.push(Problem{ .problemNum=-1});
     }
 }
 
 // This method repeatedly hashes itself until the required amount of leading zero bits is found
 inline Sha1Hash findSolutionHash(Sha1Hash hash){
     do{
-        hash = Utility::sha1(hash);
-    } while(Utility::count_leading_zero_bits(hash) < 13);
+        hash = MyUtility::sha1(hash);
+    } while(count_leading_zero_bits(hash) < 13);
 
     return hash;
 }
 
 int main() {
-    Sha1Hash solutionHashes[10000];
 
-    unsigned int seed = Utility::readInput();
+    Sha1Hash solution;
+    Sha1Hash solutionHashes[10000];
+    std::thread threads[NUM_THREADS];
+
+
+    unsigned int seed = readInput();
     std::thread generateProblems = std::thread(generateProblem, seed);
 
 
-    uint8_t num_threads = 23;
-    std::thread threads[num_threads];
-
     // create threads to solve problems
-    for (int i = 0; i < num_threads; i++) {
-        threads[i] = std::thread([i, &solutionHashes]() {
+    for (uint8_t i = 0; i < NUM_THREADS; ++i) {
+        threads[i] = std::thread([&] {
             while(true) {
                 Problem p = problemQueue.pop();
-                if (p.problemNum == -1) {
+                if (p.problemNum < 0)
                     break;
-                }
                 solutionHashes[p.problemNum] = findSolutionHash(p.sha1_hash);
             }
         });
     }
-
+    generateProblems.join();
 
     // join threads
-    for (int i = 0; i < num_threads; i++) {
+    for (uint8_t i = 0; i < NUM_THREADS; ++i) {
         threads[i].join();
     }
 
-    Sha1Hash solution;
-
-    // this doesn't need parallelization. it's fast
-    for(int i = 0; i < 10000; i++){
-        solution = Utility::sha1(solution, solutionHashes[i]);
+    for(uint16_t i = 0; i < 10000; ++i){
+        solution = MyUtility::sha1(solution, solutionHashes[i]);
     }
 
-    Utility::printHash(solution);
-    printf("DONE\n");
-    generateProblems.join();
+    printHash(solution);
+
     return 0;
 }
